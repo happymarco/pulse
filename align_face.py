beecf92af1ebbb0c8a4ed1ac59f469c004a4b00f
happymarco <marco.marchesi@happyfinish.com>
2020-06-17 13:41:45 +0100

    changed input_dir

--- align_face.py
+++ align_face.py
@@ -34,16 +34,20 @@ print("Downloading Shape Predictor")
 f=open_url("https://drive.google.com/uc?id=1huhv8PYpNNKbGCLOaYUjOgR1pY5pmbJx", cache_dir=cache_dir, return_path=True)
 predictor = dlib.shape_predictor(f)
 
-for im in Path(args.input_dir).glob("*.*"):
-    faces = align_face(str(im),predictor)
+for im in os.listdir(args.input_dir):
 
-    for i,face in enumerate(faces):
-        if(args.output_size):
-            factor = 1024//args.output_size
-            assert args.output_size*factor == 1024
-            D = BicubicDownSample(factor=factor)
-            face_tensor = torchvision.transforms.ToTensor()(face).unsqueeze(0).cuda()
-            face_tensor_lr = D(face_tensor)[0].cpu().detach().clamp(0, 1)
-            face = torchvision.transforms.ToPILImage()(face_tensor_lr)
+    if im.endswith('.png'):
+        original_im = im
+        im = os.path.join(args.input_dir, im)
+        faces = align_face(im,predictor)
 
-        face.save(Path(args.output_dir) / (im.stem+f"_{i}.png"))
+        for i,face in enumerate(faces):
+            if(args.output_size):
+                factor = 1024//args.output_size
+                assert args.output_size*factor == 1024
+                D = BicubicDownSample(factor=factor)
+                face_tensor = torchvision.transforms.ToTensor()(face).unsqueeze(0).cuda()
+                face_tensor_lr = D(face_tensor)[0].cpu().detach().clamp(0, 1)
+                face = torchvision.transforms.ToPILImage()(face_tensor_lr)
+
+            face.save(Path(args.output_dir) / original_im)
